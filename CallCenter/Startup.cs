c92eb9d1bfbc9d418c45b40f66567b2f3a8c85d8
John Batista <jc_punky@hotmail.com>
2022-12-23 18:51:49 -0500

    Agregar archivos de proyecto.

---
+++ CallCenter/Startup.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.OpenApi.Models;
+
+namespace CallCenter
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+        public void ConfigureSevices(IServiceCollection services)
+        {
+        //    services.AddDbContext<AplicationDbContext>(options =>
+        //    options.UseSqlServer(Configuration.GetConnectionString("DefaultConnection")));
+            services.AddControllers();
+            services.AddEndpointsApiExplorer();
+            services.AddSwaggerGen();
+        }
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            app.UseHttpsRedirection();
+            app.UseRouting();
+
+            app.UseAuthorization();
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+
+
+        }
+    }
+}
+}
